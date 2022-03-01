dd4293ba8badc427fd0ce99e0ab1de1a07e676d0
Charit <charithnanduri03@gmail.com>
2022-03-01 14:06:39 -0500

    added timer and block

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,10 +1,41 @@
 <template>
-  <div class="block">Click Me</div>
+  <div class="block" v-if="showBlock" @click="stopTimer">Click Me</div>
 </template>
 
 <script>
 export default {
-  props:["delay"]
+  props:["delay"],
+  data () {
+    return {
+      showBlock: false,
+      timer: null,
+      reactionTime: 0
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showBlock = true
+      this.startTimer()
+    }, this.delay)
+  },
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.reactionTime += 10
+      },10)        
+    },
+    stopTimer() {
+      clearInterval(this.timer)
+      console.log(this.reactionTime)
+      this.$emit("end",this.reactionTime)
+    }
+  },
+  updated() {
+    console.log("component updated")
+  },
+  unmounted() {
+    console.log("component unmounted")
+  }
 }
 </script>
 
@@ -17,5 +48,19 @@ export default {
         text-align: center;
         padding: 100px 0;
         margin: 40px auto;
+    }
+    button {
+      background: green;
+      color: white;
+      padding: 8px 16px;
+      font-size: 16px;
+      cursor: pointer;
+      margin: 10px;
+      border: none;
+      border-radius: 4px;
+    }
+    button[disabled] {
+      opacity: 0.2;
+      cursor: not-allowed;
     }
 </style>
---
+++ src/components/Results.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>Reaction time: {{ score }} ms</p>
+  <p class="rank">{{rank}}</p>
+</template>
+
+<script>
+export default {
+  props: ["score"],
+  data() {
+    return {
+      rank: null
+    };
+  },
+  mounted() {
+    if(this.score < 250) {
+      this.rank = "Super fast pace"
+    } else if (this.score < 400) {
+      this.rank = "fast enough"
+    } else {
+      this.rank = "slow pace"
+    }
+  }
+};
+</script>
+
+<style>
+  .rank {
+    color: rgb(140, 180, 80);
+    font-size: 1.4rem;
+    font-weight: bold;
+  }
+</style>
